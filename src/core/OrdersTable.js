5e250472d2399ef72af2c9405e222e18130b624f
Igor <example@gmail.com>
2022-03-26 18:04:16 +0300

    added class of OrdersTable

---
+++ src/core/OrdersTable.js
@@ -0,0 +1,58 @@
+import EventEmitter from "./EventEmitter.js";
+
+const orderTableTemplate = document.querySelector('[data-template="orderTable"]');
+const orderRowTemplate = document.querySelector('[data-template="orderRow"]');
+
+const priceFormater = Intl.NumberFormat('ru-RU', {
+  style: 'currency',
+  currency: 'RUB',
+});
+
+const dateFormater = Intl.DateTimeFormat('ru-RU', {
+  hour12: false,
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+});
+
+class OrdersTable extends EventEmitter {
+  $orders = [];
+  $root = null; // точка монтирования таблицы
+
+  constructor(root, orders) {
+    super();
+    this.$root = root;
+    this.$orders = orders;
+
+    this.update();
+  }
+
+  update() {
+    this.$root.textContent = '';
+    const ordersTable = orderTableTemplate.content.cloneNode(true);
+    const tbody = ordersTable.querySelector('tbody');
+
+    for (const order of this.$orders) {
+      const { id, user, orderType, price, status, createdAt } = order;
+      const { name, surname } = user;
+      
+      const orderRow = orderRowTemplate.content.cloneNode(true);
+      orderRow.querySelector('[data-field="id"]').textContent = id;
+      orderRow.querySelector('[data-field="name"]').textContent = `${name} ${surname}`;
+      orderRow.querySelector('[data-field="orderType"]').textContent = orderType;
+      orderRow.querySelector('[data-field="price"]').textContent = priceFormater.format(price);
+      orderRow.querySelector(`[data-badge="${status}"]`).classList.remove('hidden');
+      orderRow.querySelector('[data-field="createdAt"]').textContent = dateFormater.format(new Date(createdAt));
+
+      orderRow.querySelector('button').addEventListener('click', () => this.emit('edit', id));
+
+      tbody.append(orderRow);
+    }
+
+    this.$root.append(ordersTable);
+  }
+}
+
+export default OrdersTable;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import Store from "./core/Store.js";
 import Observable from "./core/Observable.js";
 import EventEmitter from "./core/EventEmitter.js";
+import OrdersTable from "./core/OrdersTable.js";
 
 const store = new Store();
 store.download();
 
-console.log(store);
 
 // const ob = new Observable();
 
@@ -14,10 +14,17 @@ console.log(store);
 
 // ob.dispatch(5);
 
-const ee = new EventEmitter();
+// const ee = new EventEmitter();
 
-ee.addEventListener('event1', (data) => console.log('event1', data));
-ee.addEventListener('event2', (data) => console.log('event2', data));
+// ee.addEventListener('event1', (data) => console.log('event1', data));
+// ee.addEventListener('event2', (data) => console.log('event2', data));
 
-ee.emit('event1', { name: 'Алексей' });
-ee.emit('event2', { name: 'Игорь' });
+// ee.emit('event1', { name: 'Алексей' });
+// ee.emit('event2', { name: 'Игорь' });
+
+const ot = new OrdersTable(
+  document.querySelector('[data-mount="ordersTable"]'),
+  store.orders.slice(0, 5)
+);
+
+ot.on('edit', (orderId) => console.log({ orderId }));
